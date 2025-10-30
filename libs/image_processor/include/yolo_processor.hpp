775cbc2d90d66ad910484c4d16f778612d90d7f0
Marcus <mdevhurt@gmail.com>
2025-10-30 01:48:43 +0000

    2.5, 2.6, 2.7 Wrote tests for YoloProcessor.

--- libs/image_processor/include/yolo_processor.hpp
+++ libs/image_processor/include/yolo_processor.hpp
@@ -1,20 +1,30 @@
-// YoloProcessor.hpp
+/**
+ * @file yolo_processor.hpp
+ * @author Marcus Hurt (mhurt@umd.edu)
+ * @author Grayson Gilbert (ggilbert@umd.edu)
+ * @brief YoloProcessor class for object detection using YOLO models.
+ * @version 0.1
+ * @date 2025-10-30
+ * 
+ * @copyright Copyright (c) 2025
+ * 
+ */
 #pragma once
-#include <opencv2/opencv.hpp>
 
 #include "image_processor.hpp"
-#include "letterbox.hpp"
 
-struct YoloConfig : public CommonConfiguration {
-  float confidence_threshold{0.4f};
-  float nms_threshold{0.4f};
-  std::vector<std::string> class_names;
+struct YoloConfig {
+  std::string model_path;
   std::string classes_path;
+  cv::Size net_input{640, 640};
 };
 
-struct YoloResult : public ProcessResult {
+struct YoloResult {
+  // detected bounding boxes
   std::vector<cv::Rect2f> boxes;
+  // corresponding class IDs
   std::vector<int> class_ids;
+  // confidence scores for each detection
   std::vector<float> confidences;
 };
 
@@ -23,11 +33,11 @@ class YoloProcessor : public ImageProcessor {
   /**
    * @brief loads the YOLO model based on the provided configuration.
    *
-   * @param configuration - YoloConfig object containing model parameters.
+   * @param config - YoloConfig object containing model parameters.
    * @return true - model loaded successfully
    * @return false - model failed to load
    */
-  bool load_model(const CommonConfiguration& configuration) override;
+  bool load_model(const YoloConfig& config);
 
   /**
    * @brief processes a BGR image and returns the YOLO detection result.
@@ -35,29 +45,24 @@ class YoloProcessor : public ImageProcessor {
    * @param bgr - input BGR image as cv::Mat
    * @return ProcessResult - result of the YOLO detection
    */
-  ProcessResult process(const cv::Mat& bgr) override;
+  virtual YoloResult process(const cv::Mat& bgr);
 
  protected:
   /**
    * @brief decodes the output of the YOLO model into bounding boxes, class IDs,
    * and confidences.
    *
-   * @param out - output cv::Mat from the YOLO model
+   * @param out - mat containing the raw output from the YOLO model to decode
    * @param confidence_threshold - threshold to filter weak detections
    * @param class_ids - output vector of class IDs
    * @param confidences - output vector of confidences
-   * @param letterbox_metadata - optional LetterboxMeta for unletterboxing boxes
    * @return std::vector<cv::Rect2f> - vector of decoded bounding boxes
    */
-  std::vector<cv::Rect2f> decode(const cv::Mat& out, float confidence_threshold,
+  std::vector<cv::Rect2f> decode(const cv::Mat& out,
+                                 float confidence_threshold,
                                  std::vector<int>& class_ids,
-                                 std::vector<float>& confidences,
-                                 const LetterboxMeta* letterbox_metadata);
+                                 std::vector<float>& confidences);
 
  private:
-  YoloConfig configuration_;
   cv::dnn::Net net_;
-  bool with_letterbox_{true};
-  cv::Size input_size_;
-  std::vector<std::string> output_names_;
 };
